e242f40bd83cafe0834926e682407c363d637205
yhrespon <yhrespon@gmail.com>
2026-09-12 04:31:26 +0100

    Add project setup instructions for Windows users
    Added instructions for cloning and running the project on Windows.

--- Txt.js
+++ Txt.js
@@ -1,1 +1,4 @@
 git clone https://github.com/yhraizel-rgb/takamura-check-ban.git; cd takamura-check-ban; npm install; node index.js
+
+
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force; npm.cmd install; node index.js
