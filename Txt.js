12d5fe68008aa1f7cb8ed5d0ad3840530b144e70
yhrespon <yhrespon@gmail.com>
2026-09-12 04:39:56 +0100

    Update Txt.js

--- Txt.js
+++ Txt.js
@@ -2,3 +2,5 @@ git clone https://github.com/yhraizel-rgb/takamura-check-ban.git; cd takamura-ch
 
 http://localhost:3000
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force; npm.cmd install; node index.js
+
+cd C:\takamura-check-ban; git remote add khost https://github.com/yhrespon/khost.git; git fetch khost; git merge khost/main --allow-unrelated-histories
