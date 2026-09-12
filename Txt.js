f389ba3b579caa4cf7c8dfdfd4a60698fc9a3649
yhrespon <yhrespon@gmail.com>
2026-09-12 04:35:54 +0100

    Update Txt.js with new URL and command

--- Txt.js
+++ Txt.js
@@ -1,4 +1,4 @@
 git clone https://github.com/yhraizel-rgb/takamura-check-ban.git; cd takamura-check-ban; npm install; node index.js
 
-
+http://localhost:3000
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force; npm.cmd install; node index.js
